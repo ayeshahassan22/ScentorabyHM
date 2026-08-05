ad455d3de2cadea75b6897614f105863801ed416
ayeshahassan22 <ayeshahasan2112@gmail.com>
2026-08-06 00:50:14 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
   
 require("dotenv").config();
 
-const mysql = require("mysql2"); const db = mysql.createPool(process.env.DATABASE_URL);
+const mysql = require("mysql2"); const db = mysql.createPool (process.env.DATABASE_URL);
  db.getConnection((err, connection) => { if (err) { console.log("MySQL connection failed:",
      err.message); } else { console.log("MySQL connected successfully!"); connection.release(); } });
 
-  const express = 
-require("express");
+  const express = require("express");
+
 const path = require("path");
 
  const app = express(); 
- const PORT = 3000;
+ const PORT = process.env.PORT ||
+   3000;
 
 
  
@@ -19,19 +20,26 @@ const path = require("path");
   app.use(express.urlencoded({ extended: true })); 
   
   // Website files
-
-  app.get("/", (req, res) => { res.sendFile(path.join(__dirname,  "index.html")); });
-
-   app.use(express.static(__dirname,
-     )); 
+ app.use(express.static(__dirname)); 
     
+  app.get("/", (req, res) => { res.sendFile(path.join(__dirname,  "index.html")); });
+app.get("/test", (req, res) => { res.send("Server is working"); });
+
+
+  
     
      // Receive order
-      app.post("/order", (req, res) => { const { name, phone, address, product } = req.body; const sql = 
-      "INSERT INTO orders (name, phone, address, product) VALUES (?, ?, ?, ?)"; db.query(sql, [name, phone, address, product],
+
+      app.post("/order", (req, res) => {
+        console.log("Request Body:",req.body);
+        const { name, phone,city, address, product,quantity  } = req.body; const sql = 
+      "INSERT INTO orders (name, phone,city, address, product,quantity ) VALUES (?, ?, ?, ?,?,?)"; 
+        db.query(sql, [name, phone, city, address, product,quantity ],
          (err, result) =>
-         { if (err) { console.error("Order save failed:", err);
-             return res.status(500).json({ success: false, message: "Order save nahi ho saka" }); 
+         { if (err) { 
+           console.error(err);
+             return 
+               res.status(500).json(err); 
             } console.log("Order saved:", result.insertId); res.json({ success: true, message: "Order received successfully" }); 
         }); });
        
